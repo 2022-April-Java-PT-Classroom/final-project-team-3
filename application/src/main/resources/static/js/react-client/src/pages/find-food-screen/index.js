ddabe161c34793aa7c59482022992cea1445035d
fatehailey <fatehailey@yahoo.com>
2022-07-24 11:34:02 -0400

    initial push

---
+++ application/src/main/resources/static/js/react-client/src/pages/find-food-screen/index.js
@@ -0,0 +1,15 @@
+import MyGoogleMap from './components/Google/MyGoogleMap.js';
+// App.js
+import React from 'react';
+import style from '../../pages/find-food-screen/style.module.scss';
+
+function App() {
+
+  return (
+    <div >
+      <MyGoogleMap />
+    </div>
+  );
+}
+
+export default App;
