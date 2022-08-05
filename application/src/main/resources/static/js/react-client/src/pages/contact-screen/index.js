e71783fd0ea664fbe48e61d46c0f2ed3a3600b7a
faridane <faridandiaye60@gmail.com>
2022-08-04 20:36:11 -0400

    updated

--- application/src/main/resources/static/js/react-client/src/pages/contact-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/contact-screen/index.js
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
-
+import React from "react";
 import style from './style.module.scss';
 
-const ContactScreen = () => {
-    
-
-    return (
-        <div className={style.ContactScreenn}>
-        <h1>Contact</h1>
+const ContactUs = () => {
+    return(
+        <div className={style.feedback}>
+            Contact info
+        <div className={style.message}>
+            <h1><span>We appreciate any feedback. Please share your thoughts with us! </span></h1>
+                <h3><a href="mailto:infocommunity2022@gmail.com">Email-Us</a></h3>
+                <p>Or Call Tool Free :</p><br></br>
+                <div>
+                    <h3>: +1 555-555-5555</h3>
+                </div>
         </div>
-        );
-}
+        </div>
+    )
 
-export default ContactScreen;
+
+}
+export default ContactUs;
