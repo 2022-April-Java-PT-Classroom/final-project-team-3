2ddbfef97fb7d0025a8dfeef6dd303d1d04c9d39
VictorLNegron <negronvictor882@gmail.com>
2022-08-07 11:26:19 -0400

    Updated article page and got the time and form to work properly

--- application/src/main/resources/static/js/react-client/src/pages/article-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/article-screen/index.js
@@ -1,5 +1,6 @@
 import React,{useEffect, useState} from "react";
 
+import ArticleForm from "./ArticleForm";
 import Axios from "axios";
 import style from './style.module.scss';
 
@@ -30,7 +31,7 @@ const Article = () => {
     return (
         <div>
           
-            <div className={style.form__container}>
+            <div >
                 
             
 
@@ -38,36 +39,18 @@ const Article = () => {
             {loadingArticle ? <h3>Loading Articles ...</h3> :
                 <>
                     <h2>Articles In Today!</h2>
-                    <ul>
-                        {article.map(article => (
-                            <div key={article.id}>
-                                
-                            
-                            </div>
-                        ))}
-                    </ul>
-                    <div className={style.container}>
+                    
+                    <ArticleForm/>
+                    <div >
                     {article.map(article => (
-                        <div className = {style.item}>
+                        <div >
                             
                             <div key={article.id}>
-                                
-                            <table >
-                                    <tr>
-                                        <td><p>Author Name: {article.authorName}</p></td>
-                                    </tr>
-                                    <tr>
-                                        <td><p>Article Title: {article.articleTitle}</p></td>
-                                    </tr>
-                                    <tr >
-                                    <td><p>Content: {article.articleBody}</p></td>
-                                    </tr>
-                                    <tr >
-                                    <td><p>date: {article.date}</p></td>
-                                        </tr>
-                                        
-                                       
-                                </table>  
+                            <p >Article Title: {article.articleTitle}</p>
+                             <p >Content: {article.articleBody}</p> 
+                             <p >Published by: {article.authorName}</p>  
+                            <p >Date Created: <br /> {article.date}</p>
+                               
                             </div>    
                             
                         </div>
